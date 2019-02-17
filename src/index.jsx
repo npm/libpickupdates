280a9915983a921a30c6f6e8ac96b8a2904b5cec
Kat Marchán <kzm@zkat.tech>
2019-02-16 23:10:11 -0800

    refactor(updater): make it a state machine

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,15 @@ import { Color, Text } from 'ink'
 import React, { useState } from 'react'
 import { List, ListItem } from './list'
 
+const STATES = {
+  init: 'init',
+  pickUpdates: 'pick-updates',
+  updating: 'updating',
+  updated: 'updated'
+}
+
 export default function UpdateInteractiveComponent ({ stdin, unicode }) {
+  const [state, setState] = useState(STATES.init)
   const [outdated, setOutdated] = useState([
     { name: 'lodash', wanted: '1.2.3', latest: '1.2.3', current: '1.2.0' },
     { name: 'libnpm', current: 'MISSING', wanted: '1.0.0', latest: '1.2.0' }
@@ -10,18 +18,25 @@ export default function UpdateInteractiveComponent ({ stdin, unicode }) {
   const [updated, setUpdated] = useState(null)
   function updateDeps (names) {
     setUpdated(names)
-    process.exit(0)
+    setState(STATES.updating)
   }
-  return (
-    !updated
-      ? <UpdatePicker
-        outdated={outdated}
-        stdin={stdin}
-        unicode={unicode}
-        onSubmit={updateDeps}
-      />
-      : <Text>Updated deps: {updated.join(', ')}</Text>
-  )
+  if (state === STATES.init) {
+    setTimeout(() => setState(STATES.pickUpdates), 2000)
+    return <Text>Loading...</Text>
+  } else if (state === STATES.pickUpdates) {
+    return <UpdatePicker
+      outdated={outdated}
+      stdin={stdin}
+      unicode={unicode}
+      onSubmit={updateDeps}
+    />
+  } else if (state === STATES.updating) {
+    setTimeout(() => setState(STATES.updated), 2000)
+    return <Text>Installing updated deps...</Text>
+  } else if (state === STATES.updated) {
+    setImmediate(() => process.exit(0))
+    return <Text>Updated deps: {updated.join(', ')}</Text>
+  }
 }
 
 function UpdatePicker ({ outdated = [], stdin, onSubmit, unicode }) {
