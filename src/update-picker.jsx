f04ed78911669a50ce1c024ac38e26032a5695b6
Kat Marchán <kzm@zkat.tech>
2019-02-20 10:30:45 -0800

    fix(sort): use right words for sorting

--- src/update-picker.jsx
+++ src/update-picker.jsx
@@ -4,9 +4,9 @@ import { Select, Option, OptGroup } from './select'
 import semver from 'semver'
 
 const types = [
-  'dependency',
-  'optionalDependency',
-  'devDependency'
+  'dependencies',
+  'optionalDependencies',
+  'devDependencies'
 ]
 export default function UpdatePicker ({ outdated = {}, stdin, onSubmit, unicode }) {
   outdated = Object.keys(outdated).map(k => {
@@ -18,9 +18,9 @@ export default function UpdatePicker ({ outdated = {}, stdin, onSubmit, unicode 
     if (x.type === y.type) {
       return 0
     } else if (types.indexOf(x.type) > types.indexOf(y.type)) {
-      return -1
-    } else {
       return 1
+    } else {
+      return -1
     }
   })
   const major = filterSemver(outdated, 'major')
